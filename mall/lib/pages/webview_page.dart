289bb65c7d49a595669a7e92b8384ac450bb4b3c
HJQ <v-huangjiaqian@midland.local>
2019-07-17 14:07:04 +0800

    webview的使用

--- mall/lib/pages/webview_page.dart
+++ mall/lib/pages/webview_page.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-/**
- * 客服
- */
-class ServerPage extends StatelessWidget {
 
-  String url = "https://www.baidu.com";
+class WebViewPage extends StatefulWidget {
+
+  String url;
+  final String title;
+
+  WebViewPage({this.url, this.title});
+
+  @override
+  _WebViewState createState() => _WebViewState();
+}
+
+class _WebViewState extends State<WebViewPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('客服'),
+        title: Text(widget.title),
       ),
-      body: WebView(
-            initialUrl: url, // 加载的url
+      body: Stack(
+        children: <Widget>[
+          new WebView(
+            initialUrl: widget.url, // 加载的url
             onWebViewCreated: (WebViewController web) {
               // webview 创建调用，
-            // web.loadUrl(url);// 此时也可以初始化一个url
+            web.loadUrl(widget.url);// 此时也可以初始化一个url
             web.canGoBack().then((res){
                 print(res); // 是否能返回上一级
              });
@@ -33,8 +51,9 @@ class ServerPage extends StatelessWidget {
             onPageFinished: (String value) {
               // webview 页面加载调用
             },
-          ),
+          )
+        ],
+      ),
     );
   }
-
-}
+ }
