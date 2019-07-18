7ca8a55be27e0ff4a8f41d6ccb86e51aadeb63f6
HJQ <v-huangjiaqian@midland.local>
2019-07-18 15:09:19 +0800

    首页功能菜单

--- mall/lib/widgets/home/home_items.dart
+++ mall/lib/widgets/home/home_items.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 /**
  * 首页的功能菜单
@@ -14,12 +15,50 @@ class HomeItems extends StatefulWidget {
 }
 
 class _HomeItemsState extends State<HomeItems> {
+
+ Widget _swiperBuilder(BuildContext context) {
+
+    return Container(
+      child: new ConstrainedBox(
+        child: new Swiper(
+          outer:false,
+          itemBuilder: (c, i) {
+            return new Wrap(
+              runSpacing:  6.0,
+              children: [0,1,2,3,4,5,6,7,8,9].map((i){
+                return new SizedBox(
+                  width: MediaQuery.of(context).size.width/5,
+                  child: new Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new SizedBox(
+                        child:  new Container(
+                          child: new Image.network("https://fuss10.elemecdn.com/c/db/d20d49e5029281b9b73db1c5ec6f9jpeg.jpeg%3FimageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90"),
+                        ),
+                        height: MediaQuery.of(context).size.width * 0.12,
+                        width: MediaQuery.of(context).size.width * 0.12,
+                      ),
+                      new Padding(padding: new EdgeInsets.only(top:6.0),child: new Text("$i"),)
+                    ],
+                  ),
+                );
+              }).toList(),
+            );
+          },
+          pagination: new SwiperPagination(
+            margin: new EdgeInsets.all(5.0)
+          ),
+          itemCount: 3,
+        ),
+          constraints:new BoxConstraints.loose(new Size(MediaQuery.of(context).size.width, 200.0))
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      child: Text('功能菜单'),
-    );
+    return _swiperBuilder(context);
   }
 
 }
